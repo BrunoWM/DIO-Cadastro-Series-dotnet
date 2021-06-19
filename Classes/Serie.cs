1398f4c4f087717a530905e2adf2407e182ee3eb
BrunoWM <bwillmoura3595@gmail.com>
2021-06-19 19:12:20 -0300

    Criacao de classes.

---
+++ Classes/Serie.cs
@@ -0,0 +1,10 @@
+namespace DIO_Cadastro_Series_dotnet.Classes
+{
+    public class Serie
+    {
+        private Genero Genero {get; set;}
+        private string Titulo {get; set;}
+        private string Descricao {get; set;}
+        private int ano {get; set;}
+    }
+}
