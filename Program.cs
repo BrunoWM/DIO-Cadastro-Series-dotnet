82f66bd8a36a3d7fb8001eff482f87deb8c1b585
BrunoWM <bwillmoura3595@gmail.com>
2021-06-19 18:52:14 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DIO_Cadastro_Series_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
