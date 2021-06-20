dc97f00c5fb706f4497c2d472e2a0187fe5f74e7
BrunoWM <bwillmoura3595@gmail.com>
2021-06-20 18:19:26 -0300

    Menu usuário.

--- Program.cs
+++ Program.cs
@@ -8,5 +8,24 @@ namespace DIO_Cadastro_Series_dotnet
         {
             Console.WriteLine("Hello World!");
         }
+
+        private static string ObterOpcaoUsuario() {
+            Console.WriteLine();
+            Console.WriteLine("Will Séries a seu dispor!");
+            Console.WriteLine("Informe a opção desejada:");
+
+            Console.WriteLine("1 - Listar séries.");
+            Console.WriteLine("2 - Inserir nova série.");
+            Console.WriteLine("3 - Atualizar série.");
+            Console.WriteLine("4 - Excluir série.");
+            Console.WriteLine("5 - Visualizar série.");
+            Console.WriteLine("C - Limpar tela.");
+            Console.WriteLine("X - Sair.");
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            
+            return opcaoUsuario;
+        }
     }
 }
